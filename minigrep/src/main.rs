27326469520d5c3e3f2d26f6a715f880b6f40086
Anshul <anshulporwal2001@gmail.com>
2026-06-21 20:41:00 +0530

    minigrep: file search CLI with case-sensitive/insensitive modes

---
+++ minigrep/src/main.rs
@@ -0,0 +1,60 @@
+use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+use minigrep::search_case_sensitive;
+use minigrep::search_case_insensitive;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // the std::env::args function returns an iterator of the command line arguments passed to minigrep
+    // the collect method on an iterator turns it into a collection (such as vector)
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search_case_sensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough parameters");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
+    }
+}
+
+
